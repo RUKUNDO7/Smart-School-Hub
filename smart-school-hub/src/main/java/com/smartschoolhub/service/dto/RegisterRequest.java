d532292987e1325983dae9534ea6a335b5ca4b22
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-16 11:54:41 +0200

    Added register requesr DTO

---
+++ smart-school-hub/src/main/java/com/smartschoolhub/service/dto/RegisterRequest.java
@@ -0,0 +1,60 @@
+﻿package com.smartschoolhub.service.dto;
+
+import com.smartschoolhub.domain.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class RegisterRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotNull
+    private Role role;
+
+    private Long studentId;
+
+    private Long teacherId;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+}
